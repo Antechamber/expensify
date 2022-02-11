7bf2ed6843fcbe28ecf35542772048f3d4265711
Brian Day <antechamber@Brians-MacBook-Pro.local>
2022-02-10 20:09:02 -0800

    prepared firebase integration for heroku deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getDatabase } from 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjiNWmk7kinVxdh9pVrgU9tchYXjcQa4o",
-  authDomain: "expensify-10c03.firebaseapp.com",
-  databaseURL: "https://expensify-10c03-default-rtdb.firebaseio.com",
-  projectId: "expensify-10c03",
-  storageBucket: "expensify-10c03.appspot.com",
-  messagingSenderId: "711361767444",
-  appId: "1:711361767444:web:5d1135b42a9e0ed1899f01"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABARE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -42,65 +42,60 @@ test('Should generate ADD_EXPENSE action object', () => {
   })
 })
 
-// test('Should add expense to database and store', (done) => {
-//   const store = createMockStore({})
-//   const expenseData = {
-//     description: 'New Mac',
-//     amount: 50000,
-//     note: '',
-//     createdAt: 10000
-//   }
-//   store.dispatch(startAddExpense(expenseData)).then(() => {
-//     const actions = store.getActions()
-//     expect(actions[0]).toEqual({
-//       type: 'ADD_EXPENSE',
-//       expense: {
-//         id: expect.any(String),
-//         ...expenseData
-//       }
-//     })
+test('Should add expense to database and store', (done) => {
+  const store = createMockStore({})
+  const expenseData = {
+    description: 'New Mac',
+    amount: 50000,
+    note: '',
+    createdAt: 10000
+  }
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    })
 
-//     let data
+    onValue(ref(db, `expenses/${actions[0].expense.id}`), (snapshot) => {
+      expect(snapshot.val()).toEqual({
+        ...expenseData
+      })
+      done()
+    }, { onlyOnce: true })
 
-//     onValue(ref(db, `expenses/${actions[0].expense.id}`), (snapshot) => {
-//       data = snapshot.val()
-//     }, { onlyOnce: true })
-//   }).then((data) => {
-//     expect(data).toEqual(expenseData)
-//     done()
-//   })
-// })
+  })
+})
 
-// test('Should add expense to database and store with default values', () => {
-//   const store = createMockStore([])
-//   store.dispatch(startAddExpense()).then(() => {
-//     const actions = store.getActions()
-//     expect(actions[0]).toEqual({
-//       type: 'ADD_EXPENSE',
-//       expense: {
-//         id: expect.any(String),
-//         description: '',
-//         note: '',
-//         amount: 0,
-//         createdAt: 0
-//       }
-//     })
-//     done()
-//   }).catch((e) => {
-//     console.log(e)
-//   })
-// })
+test('Should add expense with default values to database and store', (done) => {
+  const store = createMockStore({})
+  const expenseData = {}
 
-// test('Should generate ADD_EXPENSE action object (default values)', () => {
-//     const action = addExpense()
-//     expect(action).toMatchObject({
-//         type: 'ADD_EXPENSE',
-//         expense: {
-//             id: expect.any(String),
-//             description: '',
-//             note: '',
-//             amount: 0,
-//             createdAt: 0
-//         }
-//     })
-// })
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0
+      }
+    })
+
+    onValue(ref(db, `expenses/${actions[0].expense.id}`), (snapshot) => {
+      expect(snapshot.val()).toEqual({
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0
+      })
+      done()
+    }, { onlyOnce: true })
+
+  })
+})
